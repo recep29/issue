d1d9e2dca2f375f28744c17ef5d6a453fb22b802
recep29 <smyyrcp29>
2019-12-01 00:46:08 +0300

    json dan server ayağa kalkarken ornek project verisi atn kod eklendi. project.json + bean kodu. spring Jackson

--- src/main/java/com/gulrecep/issuemanegement/IssueManegementApplication.java
+++ src/main/java/com/gulrecep/issuemanegement/IssueManegementApplication.java
@@ -2,26 +2,35 @@ package com.gulrecep.issuemanegement;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
 public class IssueManegementApplication {
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(IssueManegementApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(IssueManegementApplication.class, args);
 
-	}
+    }
 
-	@Bean
-	public ModelMapper getModelMapper() {
-		ModelMapper modelMapper= new ModelMapper();
-		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-		return modelMapper;
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
 
-	}
+    }
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean repositoryPopulatorFactoryBean() {
+        Jackson2RepositoryPopulatorFactoryBean factoryBean = new Jackson2RepositoryPopulatorFactoryBean();
+        factoryBean.setResources(new Resource[]{new ClassPathResource("project.json")});
+        return factoryBean;
+    }
 
 }
